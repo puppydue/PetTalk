0a7a7ed20f40059b74fdd1c7ba64b8be20dc46f8
giaminh2005 <nhngminh669@gmail.com>
2025-10-30 20:26:38 +0700

    updated forum , forum.css

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'forum/post_list.html')
+    posts = Post.objects.prefetch_related('images', 'comments', 'reactions').all()
+
+    # Thêm dữ liệu đếm reaction cho mỗi post
+    for post in posts:
+        post.upvotes = post.reactions.filter(type='upvote').count()
+        post.downvotes = post.reactions.filter(type='downvote').count()
+
+    return render(request, 'forum/post_list.html', {'posts': posts})
