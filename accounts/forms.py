223e6420431664e5ecf6082bc8741cb08f0dcb5d
puppydue <dinhkhanh148@gmail.com>
2025-11-02 21:41:16 +0700

    update quên mật khẩu + trang đăng nhập by KHÁNH

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,31 +1,46 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Tên người dùng',
-        'class': 'input-field',
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Mật khẩu',
-        'class': 'input-field',
-    }))
+    username = forms.CharField(
+        label="",
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Tên người dùng',
+            'class': 'input-field'
+        })
+    )
+    password = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Mật khẩu',
+            'class': 'input-field'
+        })
+    )
 
 class CustomRegisterForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'placeholder': 'Email',
-        'class': 'input-field',
-    }))
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Tên người dùng',
-        'class': 'input-field',
-    }))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Mật khẩu',
-        'class': 'input-field',
-    }))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Nhập lại mật khẩu',
-        'class': 'input-field',
-    }))
+    username = forms.CharField(
+        label="",
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Tên người dùng',
+            'class': 'input-field'
+        })
+    )
+    password1 = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Mật khẩu',
+            'class': 'input-field'
+        })
+    )
+    password2 = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Nhập lại mật khẩu',
+            'class': 'input-field'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import CustomRegisterForm, CustomLoginForm
 from django.contrib import messages
 
 
 def auth_slider(request):
-    """Trang đăng nhập & đăng ký (chung giao diện)"""
-    login_form = AuthenticationForm()
-    register_form = UserCreationForm()
+    login_form = CustomLoginForm()
+    register_form = CustomRegisterForm()
 
     if request.method == 'POST':
         if 'login' in request.POST:
-            login_form = AuthenticationForm(request, data=request.POST)
+            login_form = CustomLoginForm(request, data=request.POST)
             if login_form.is_valid():
                 user = login_form.get_user()
                 login(request, user)
@@ -21,7 +20,7 @@ def auth_slider(request):
                 messages.error(request, "Sai tên đăng nhập hoặc mật khẩu.")
 
         elif 'register' in request.POST:
-            register_form = UserCreationForm(request.POST)
+            register_form = CustomRegisterForm(request.POST)
             if register_form.is_valid():
                 register_form.save()
                 messages.success(request, "Tạo tài khoản thành công! Hãy đăng nhập.")
